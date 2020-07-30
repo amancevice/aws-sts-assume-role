2545eae8fcf7e33d67aad2e66cd5ac37a8cfcecb
Alexander Mancevice <alexander.mancevice@hey.com>
2020-07-30 07:48:14 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# aws-sts-assume-role
-GitHub Action to assume an AWS IAM role and export the credentials to ENV
+# AWS Assume Role GitHub Action
+
+Assume an AWS IAM role and export the credentials to your GitHub Action ENV.
+
+```yaml
+- uses: amancevice/aws-sts-assume-role@main
+  with:
+    access_key_id:     ${{ secrets.AWS_ACCESS_KEY_ID }}
+    secret_access_key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+    role_arn:          ${{ secrets.AWS_ROLE_ARN }}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const { STS } = require('aws-sdk');
+
+const assumeRole = async () => {
+  try {
+    // Gather GitHub Actions inputs
+    const access_key_id     = core.getInput('access_key_id');
+    const secret_access_key = core.getInput('secret_access_key');
+    const duration_seconds  = core.getInput('duration_seconds');
+    const role_arn          = core.getInput('role_arn');
+    const role_session_name = core.getInput('role_session_name');
+
+    // Create STS client
+    const sts = new STS({
+      accessKeyId:     access_key_id,
+      secretAccessKey: secret_access_key,
+    });
+
+    // Assume role with STS
+    const res = await sts.assumeRole({
+      RoleArn:         role_arn,
+      RoleSessionName: role_session_name,
+      DurationSeconds: duration_seconds,
+    }).promise()
+
+    // Export ENV
+    core.exportVariable('AWS_ACCESS_KEY_ID',     res.Credentials.AccessKeyId);
+    core.exportVariable('AWS_SECRET_ACCESS_KEY', res.Credentials.SecretAccessKey);
+    core.exportVariable('AWS_SESSION_TOKEN',     res.Credentials.SessionToken);
+  } catch (error) {
+    core.error(error);
+    throw new Error(error);
+  }
+}
+
+const main = async () => {
+  try {
+    await assumeRole();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
